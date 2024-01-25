68a07242c1147cc12afc657c782fc0465dda8d36
Andrej Nenadić <andrejnana445@gmail.com>
2024-01-25 21:07:47 +0100

    Made each separated image a button and gave it a click event listener which just logs the position of its texture in the material

--- Assets/Code/TileMap/Testing.cs
+++ Assets/Code/TileMap/Testing.cs
@@ -13,6 +13,9 @@ namespace Assets.Code.TileMap
 
         private void Start()
         {
+            Debug.Log("Start");
+            baseImage.GetComponent<Button>().onClick.AddListener(() => Debug.Log("AAA")); ;
+
             Grid<TileMapNode> grid = new(50, 50, 1, (g, x, y) => new(x, y, new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)), new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)), true));
             var mesh = grid.CreateMesh();
             GetComponent<MeshFilter>().mesh = mesh;
@@ -28,15 +31,24 @@ namespace Assets.Code.TileMap
             for (int i = 0; i < tiles; i++)
             {
                 var newImage = Instantiate(baseImage, baseImageLocation + new Vector3(baseimageWidth * i, 0), Quaternion.identity, canvas.transform);
+                Vector2 offset = new(i * (1f / tiles) + .01f, 0);
 
                 var newMaterial = new Material(baseImageMaterial)
                 {
                     mainTextureScale = tileTextureScale,
-                    mainTextureOffset = new(i * (1f / tiles) + .01f, 0)
+                    mainTextureOffset = offset
                 };
 
                 newImage.GetComponent<Image>().material = newMaterial;
+                newImage.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    Debug.Log(offset);
+                });
             }
+        }
+        public void Test()
+        {
+            Debug.Log("Click");
         }
     }
 }

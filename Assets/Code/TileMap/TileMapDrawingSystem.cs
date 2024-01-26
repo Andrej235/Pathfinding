d807e0c5ccd4bbf9282e2ab1e468b4ebae8f0b74
Andrej Nenadić <andrejnana445@gmail.com>
2024-01-27 00:19:00 +0100

    Created documentation for TileMapDrawingSystem, refactored TileMapDrawingSystem a bit and added the DebuggerExtensions class in Utility folder

--- Assets/Code/TileMap/TileMapDrawingSystem.cs
+++ Assets/Code/TileMap/TileMapDrawingSystem.cs
@@ -1,13 +1,12 @@
 ﻿using Assets.Code.Grid;
+using Assets.Code.Utility;
 using CodeMonkey.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Windows;
 using Directory = System.IO.Directory;
 using File = System.IO.File;
 using Input = UnityEngine.Input;
@@ -33,7 +32,6 @@ namespace Assets.Code.TileMap
         public Grid<TileMapNode> Grid { get; private set; }
         private Vector2 selectedUVValue = new();
         private bool isColliderToggleChecked;
-        private bool areMeshesLoaded = false;
 
         private void Start()
         {
@@ -50,57 +48,43 @@ namespace Assets.Code.TileMap
 
             colliderToggle.onValueChanged.AddListener(ColliderToggleValueChanged);
 
-            areMeshesLoaded = true;
-            CreateGrid();
             IntializeTileMapButtons();
-        }
-
-        private void OnValidate()
-        {
-            if (Application.isPlaying && areMeshesLoaded)
-                CreateGrid();
         }
 
         public void CreateGrid()
         {
-            if (Grid != null)
-            {
-                if (Grid.Height != height || Grid.Width != width)
-                {
-                    var newGrid = new Grid<TileMapNode>(width, height, cellSize, (g, x, y) => new(x, y, Vector2.zero, Vector2.zero, true));
-                    var minWidth = Math.Min(Grid.Width, width);
-                    var minHeight = Math.Min(Grid.Height, height);
-
-                    for (int i = 1; i < minWidth - 1; i++)
-                    {
-                        for (int j = 1; j < minHeight - 1; j++)
-                        {
-                            newGrid[i, j].UV00 = Grid[i, j].UV00;
-                            newGrid[i, j].UV11 = Grid[i, j].UV11;
-                            newGrid[i, j].isWalkable = Grid[i, j].isWalkable;
-                        }
-                    }
-
-                    Grid = newGrid;
-                    Grid.CreateOuterWalls();
-                    GenerateMesh();
-                    GenerateColliders();
-                }
-
-                if (Grid.CellSize != cellSize)
-                {
-                    Grid.CellSize = cellSize;
-                    GenerateMesh();
-                    GenerateColliders();
-                }
+            if (Grid == null)
                 return;
+
+            if (Grid.Height != height || Grid.Width != width)
+            {
+                var newGrid = new Grid<TileMapNode>(width, height, cellSize, (g, x, y) => new(x, y, Vector2.zero, Vector2.zero, true));
+                var minWidth = Math.Min(Grid.Width, width);
+                var minHeight = Math.Min(Grid.Height, height);
+
+                for (int i = 1; i < minWidth - 1; i++)
+                {
+                    for (int j = 1; j < minHeight - 1; j++)
+                    {
+                        newGrid[i, j].UV00 = Grid[i, j].UV00;
+                        newGrid[i, j].UV11 = Grid[i, j].UV11;
+                        newGrid[i, j].isWalkable = Grid[i, j].isWalkable;
+                    }
+                }
+
+                Grid = newGrid;
+                Grid.CreateOuterWalls();
+                GenerateMesh();
+                GenerateColliders();
             }
 
-            /*            Grid = new(width, height, cellSize, (g, x, y) => new(x, y, Vector2.zero, Vector2.zero, true));
-                        Grid.CreateOuterWalls();
-
-                        GenerateMesh();
-                        GenerateColliders();*/
+            if (Grid.CellSize != cellSize)
+            {
+                Grid.CellSize = cellSize;
+                GenerateMesh();
+                GenerateColliders();
+            }
+            return;
         }
 
         public void SetGrid(Grid<TileMapNode> grid)
@@ -151,7 +135,7 @@ namespace Assets.Code.TileMap
 
                 if (isColliderToggleChecked)
                 {
-                    Grid[x, y].isWalkable = false; //!grid[x, y].isWalkable;
+                    Grid[x, y].isWalkable = false;
                     GenerateColliders();
                 }
             }
@@ -206,8 +190,6 @@ namespace Assets.Code.TileMap
     [CustomEditor(typeof(TileMapDrawingSystem))]
     public class TileMapDrawingSystemEditor : Editor
     {
-        private DungeonScriptableObject dungeon;
-
         public override void OnInspectorGUI()
         {
             var system = (TileMapDrawingSystem)target;
@@ -285,10 +267,6 @@ namespace Assets.Code.TileMap
 
                     Grid<TileMapNode> Grid = new(system.width, system.height, system.cellSize, (g, x, y) => new(x, y, Vector2.zero, Vector2.zero, true));
                     system.SetGrid(Grid);
-                    Grid.CreateOuterWalls();
-
-                    system.GenerateMesh();
-                    system.GenerateColliders();
 
                     for (int i = 0; i < system.width; i++)
                     {
@@ -304,46 +282,53 @@ namespace Assets.Code.TileMap
                     system.GenerateMesh();
                     system.GenerateColliders();
                 }
-                catch (Exception e)
+                catch (Exception ex)
                 {
-                    Debug.Log(e.InnerException.Message + "-----> : " + e);
+                    ex.LogError();
                 }
             }
 
             if (GUILayout.Button("Save"))
             {
-                List<List<Vector2>> uv00s = new();
-                List<List<Vector2>> uv11s = new();
-
-                List<List<bool>> walkable = new();
-
-
-                for (int i = 0; i < system.width; i++)
+                try
                 {
-                    uv00s.Add(new());
-                    uv11s.Add(new());
-                    walkable.Add(new());
+                    List<List<Vector2>> uv00s = new();
+                    List<List<Vector2>> uv11s = new();
 
-                    for (int j = 0; j < system.height; j++)
+                    List<List<bool>> walkable = new();
+
+
+                    for (int i = 0; i < system.width; i++)
                     {
-                        uv00s[i].Add(system.Grid[i, j].UV00);
-                        uv11s[i].Add(system.Grid[i, j].UV11);
+                        uv00s.Add(new());
+                        uv11s.Add(new());
+                        walkable.Add(new());
 
-                        walkable[i].Add(system.Grid[i, j].isWalkable);
+                        for (int j = 0; j < system.height; j++)
+                        {
+                            uv00s[i].Add(system.Grid[i, j].UV00);
+                            uv11s[i].Add(system.Grid[i, j].UV11);
+
+                            walkable[i].Add(system.Grid[i, j].isWalkable);
+                        }
                     }
+
+                    TileMapGridDTO dto = new(
+                        gridWidth: system.width,
+                        gridHeight: system.height,
+                        gridCellSize: system.cellSize,
+                        gridUV00s: uv00s,
+                        gridUV11s: uv11s,
+                        gridIsWalkables: walkable
+                        );
+
+                    string gridJsonFile = JsonUtility.ToJson(dto);
+                    File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), @"Assets\JSONFiles\asdasd.json"), gridJsonFile);
                 }
-
-                TileMapGridDTO dto = new(
-                    gridWidth: system.width,
-                    gridHeight: system.height,
-                    gridCellSize: system.cellSize,
-                    gridUV00s: uv00s,
-                    gridUV11s: uv11s,
-                    gridIsWalkables: walkable
-                    );
-
-                string gridJsonFile = JsonUtility.ToJson(dto);
-                File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), @"Assets\JSONFiles\asdasd.json"), gridJsonFile);
+                catch (Exception ex)
+                {
+                    ex.LogError();
+                }
             }
 
             if (GUILayout.Button("Create a new grid"))
--- Assets/Code/TileMap/TileMapLoadingService.cs
+++ Assets/Code/TileMap/TileMapLoadingService.cs
@@ -7,6 +7,18 @@ namespace Assets.Code.TileMap
 {
     public static class TileMapLoadingService
     {
+        /// <summary>
+        /// Creates a mesh which represents the given grid by drawing quads in positions of each grid cell based on their UV00 and UV11 values
+        /// </summary>
+        /// <param name="grid">Grid which will be used for generating a mesh using it's UV00 and UV11 coordinates</param>
+        /// <param name="mesh">
+        /// If null, creates a new one
+        /// <br />If not null, changes the existing / given mesh
+        /// </param>
+        /// <returns>
+        /// If mesh is null returns a new mesh containing the appropriate
+        /// If mesh is not null returns the edited mesh containing the appropriate
+        /// </returns>
         public static Mesh CreateMesh<T>(this IGrid<T> grid, Mesh mesh = null) where T : TileMapNode
         {
             Vector2 quadSize = new(grid.CellSize, grid.CellSize);
@@ -29,6 +41,13 @@ namespace Assets.Code.TileMap
             return mesh;
         }
 
+        /// <summary>
+        /// Create a mesh which represents the colliders of the given grid.
+        /// <br />Both UV00 and UV11 will be set to Vector2(1, 0) if isWalkable and Vector2(0, 0) if not
+        /// </summary>
+        /// <param name="grid">Grid which will be used for generating a mesh using it's isWalkable property (Vector2(1, 0) if isWalkable and Vector2(0, 0) if not)</param>
+        /// <param name="excludeWalkable">If true, only generates the mesh containing quads (cells) which are walkable</param>
+        /// <returns>Newly generated mesh</returns>
         public static Mesh CreateColliderMesh<T>(this IGrid<T> grid, bool excludeWalkable = false) where T : PathNode
         {
             try
@@ -74,6 +93,12 @@ namespace Assets.Code.TileMap
             }
         }
 
+        /// <summary>
+        /// Creates colliders on appropriate cell positions of a given grid (non-walkable cells)
+        /// </summary>
+        /// <param name="grid">Grid which will be used for generating a mesh using it's isWalkable property</param>
+        /// <param name="parent">Newly created objects will have this parent asigned to them, null means no parent</param>
+        /// <returns>HashSet containing all the newly created gameobjects (collider holders)</returns>
         public static HashSet<GameObject> CreateColliders<T>(this IGrid<T> grid, Transform parent = null) where T : PathNode
         {
             HashSet<GameObject> colliders = new();
@@ -99,36 +124,47 @@ namespace Assets.Code.TileMap
             return colliders;
         }
 
+        /// <summary>
+        /// Creates outer walls / boundaries of the grid and asigns the colliders to them
+        /// </summary>
         public static void CreateOuterWalls<T>(this IGrid<T> grid) where T : TileMapNode
         {
             if (grid.Height > 0)
             {
+                //Goes through the lower edge of the grid
                 for (int i = 0; i < grid.Width; i++)
                 {
                     grid[i, 0].UV00 = new(1, 1);
                     grid[i, 0].UV11 = new(1, 1);
+                    grid[i, 0].isWalkable = false;
                 }
 
+                //Goes through the upper edge of the grid
                 for (int i = 0; i < grid.Width; i++)
                 {
                     grid[i, grid.Height - 1].UV00 = new(1, 1);
                     grid[i, grid.Height - 1].UV11 = new(1, 1);
-                } 
+                    grid[i, grid.Height - 1].isWalkable = false;
+                }
             }
 
             if (grid.Width > 0)
             {
+                //Goes through the left edge of the grid
                 for (int i = 0; i < grid.Height; i++)
                 {
                     grid[0, i].UV00 = new(1, 1);
                     grid[0, i].UV11 = new(1, 1);
+                    grid[0, i].isWalkable = false;
                 }
 
+                //Goes through the right edge of the grid
                 for (int i = 0; i < grid.Height; i++)
                 {
                     grid[grid.Width - 1, i].UV00 = new(1, 1);
                     grid[grid.Width - 1, i].UV11 = new(1, 1);
-                } 
+                    grid[grid.Width - 1, i].isWalkable = false;
+                }
             }
         }
     }

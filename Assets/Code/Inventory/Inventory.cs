0a847437a984844f9e5c75042bf8025344832d44
Andrej Nenadić <andrejnana445@gmail.com>
2024-01-30 18:22:32 +0100

    Made inventory class, added a few more interfaces

---
+++ Assets/Code/Inventory/Inventory.cs
@@ -0,0 +1,14 @@
+using Assets.Code.Items.Interfaces;
+using System.Collections.Generic;
+
+namespace Assets.Code.Inventory
+{
+    public class Inventory
+    {
+        public IWeapon Weapon { get; protected set; }
+        public IAbility RightAbility { get; protected set; }
+        public IAbility LeftAbility { get; protected set; }
+        public List<IItem> Storage { get; protected set; }
+        public List<IAccessory> Accessories { get; protected set; }
+    }
+}

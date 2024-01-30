c6e8ca335bba2639a8a43341f37ba514303181aa
andjelic06aleksa@gmail.com <andjelic06aleksa@gmail.com>
2024-01-30 22:52:51 +0100

    Made basic InventoryUI

---
+++ Assets/Code/InventoryUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] private GameObject UIInventory;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            UIInventory.SetActive(!UIInventory.activeSelf);
+        }
+    }
+}
--- Assets/Code/Movement/FrameBasedMovement.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class FrameBasedMovement : MonoBehaviour, IMovement
-{
-    [SerializeField] private float speed;
-    public void MoveTo(Vector2 targetPos) => transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-    public void MoveBy(float x, float y) => MoveTo(transform.position + new Vector3(x, y));
-    public void MoveBy(Vector2 moveBy) => MoveTo((Vector2)transform.position + moveBy);
-}
